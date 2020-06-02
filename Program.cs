06493104978557bee63f3204c65bb18ef8abd099
bcs173110 <63640359+bcs173110@users.noreply.github.com>
2020-06-02 12:54:00 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Practic_task2
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            Console.WriteLine("Enter Input Match String ");
+           
+            a = Console.ReadLine(); 
+
+            {
+                if (a == "morning")
+                {
+
+                    Console.WriteLine("good morning ");
+                }
+
+                else if (a == "afternoon")
+                {
+
+                    Console.WriteLine("Good Afternoon");
+                }
+                else 
+                {
+
+                    Console.WriteLine("Good Night");
+                }
+            }
+        }
+    }
+}
+
